1c4d34c679a31a5e5e9ac9cea92bc4323d74a083
kolda2014 <98867069+kolda2014@users.noreply.github.com>
2023-05-29 05:14:09 +0700

    created a working surface for the schemes and its basis

---
+++ MyCOOLproject/Models/MyShemVkladka.cs
@@ -0,0 +1,34 @@
+﻿using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCOOLproject.Models
+{
+    public class MyShemVkladka:AbstractNotifyPropertyChanged
+    {
+        private string nameVklda;
+        private ObservableCollection<ObjectInShem> elements;
+
+        public MyShemVkladka()
+        {
+            CreateNameVkladka = "NoName";
+            Elements = new ObservableCollection<ObjectInShem>();
+        }
+
+        public string CreateNameVkladka
+        {
+            get => nameVklda;
+            set => SetAndRaise(ref nameVklda, value);
+        }
+
+        public ObservableCollection<ObjectInShem> Elements
+        {
+            get => elements;
+            set => SetAndRaise(ref elements, value);
+        }
+    }
+}
---
+++ MyCOOLproject/Models/ObjectInShem.cs
@@ -0,0 +1,78 @@
+﻿using Avalonia;
+using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCOOLproject.Models
+{
+    public class ObjectInShem: AbstractNotifyPropertyChanged
+    {
+        protected Avalonia.Point StartPoint;
+        public event EventHandler<CheckLine> NewStartPoint;
+        public event EventHandler<CheackStatus> NewStatus;
+        public Avalonia.Point StartPointObject
+        {
+            get => StartPoint;
+            set
+            {
+                Point oldPoint = StartPointObject;
+                SetAndRaise(ref StartPoint, value);
+                if (NewStartPoint != null)
+                {
+                    CheckLine args = new CheckLine
+                    {
+                        OldStartPoint = oldPoint,
+                        NewStartPoint = StartPointObject,
+                    };
+                    NewStartPoint(this, args);
+                }
+
+            }
+        }
+
+        public int OutValueElement;
+        public int ExitValue
+        {
+            get => OutValueElement;
+            set
+            {
+                SetAndRaise(ref OutValueElement, value);
+                if (NewStatus != null)
+                {
+                    CheackStatus args = new CheackStatus
+                    {
+                        Status = ExitValue
+                    };
+                    NewStatus(this, args);
+                }
+            }
+        }
+
+        public int ExitValue2
+        {
+            get => OutValueElement;
+            set
+            {
+                SetAndRaise(ref OutValueElement, value);
+                if (NewStatus != null)
+                {
+                    CheackStatus args = new CheackStatus
+                    {
+                        Status = ExitValue2
+                    };
+                    NewStatus(this, args);
+                }
+            }
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
